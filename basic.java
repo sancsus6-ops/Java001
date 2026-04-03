7541e9f8883be29bf569e58dd208a21760f50682
Sancsus6-ops <sancsus6@gmail.com>
2026-04-03 22:29:26 +0100

    my first java code

---
+++ basic.java
@@ -0,0 +1,37 @@
+public class basic{
+
+public static void main(String[] args) {
+// int x  = 7;
+// double y = 5.5;
+  //int y==x
+//String myName = "GabNix";
+// String studentName = "GabNix";
+//char grade = 'A';
+// int age = 25;
+// double GPA = 5.00;
+//boolean isGraduated = true;  
+//boolean show = (5!=num);
+//double fig = 6.8;
+//boolean show = (num==grade);
+//String accountName = "GabNix";
+//double iniBalance = 500.00;
+//double deposit = 500.00;
+//double cashOut = 200.00;
+//double balAfterDeposit = iniBalance + deposit;
+//double currentBalance = balAfterDeposit - cashOut;
+
+int age = 3;
+if(age >= 5);
+System.out.println("you be boss");
+else (age =< 3);
+system.out.println("youre still a kid");
+ 
+
+
+//System.out.println("Accouunt name:" +accountName+ ",initial balance: "+iniBalance+ ", withdrawal amount: "+cashOut+", currentbalance:"+currentBalance);
+
+}
+
+}
+
+
