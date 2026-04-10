158be11af58236c0cbdb3ec87e9441b747ed425c
Sancsus6-ops <sancsus6@gmail.com>
2026-04-10 18:06:01 +0100

    my control flow java exercise

--- basic.java
+++ basic.java
@@ -1,34 +1,12 @@
 public class basic{
 
 public static void main(String[] args) {
-// int x  = 7;
-// double y = 5.5;
-  //int y==x
-//String myName = "GabNix";
-// String studentName = "GabNix";
-//char grade = 'A';
-// int age = 25;
-// double GPA = 5.00;
-//boolean isGraduated = true;  
-//boolean show = (5!=num);
-//double fig = 6.8;
-//boolean show = (num==grade);
-//String accountName = "GabNix";
-//double iniBalance = 500.00;
-//double deposit = 500.00;
-//double cashOut = 200.00;
-//double balAfterDeposit = iniBalance + deposit;
-//double currentBalance = balAfterDeposit - cashOut;
+ int age = 18;
+ if (age=>);
+System.out.println(you na boss);
+//else;
+//System.out.println(you be smalli);
 
-int age = 3;
-if(age >= 5);
-System.out.println("you be boss");
-else (age =< 3);
-system.out.println("youre still a kid");
- 
-
-
-//System.out.println("Accouunt name:" +accountName+ ",initial balance: "+iniBalance+ ", withdrawal amount: "+cashOut+", currentbalance:"+currentBalance);
 
 }
 
---
+++ control.java
@@ -0,0 +1,30 @@
+public class control {
+ public static void main(String[] args){
+//int age = 10;
+//if(age <= 6){
+//System.out.println("youre a kid");
+//}else if(age >= 10){
+//System.out.println("youre improving");
+//}else{
+//System.out.println("who are you");
+//}
+//}
+//int grade = 70;
+//if(grade >= 70){
+//System.out.println("your grade is A");
+//} else if(grade <= 69){
+//System.out.println("your grade is B");
+//}else{
+//System.out.println("your grade is F sorry");
+String username = "admin";
+String password = "1234";
+if(username.equals("admin")&& password.equals("1234")){
+System.out.println("login successful");
+}else{
+System.out.println("invalid details");
+} 
+
+}
+
+}
+
