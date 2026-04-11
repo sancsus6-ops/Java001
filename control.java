aae9a4b5692b4de8c02c03110b94f22f04c90591
Sancsus6-ops <sancsus6@gmail.com>
2026-04-11 17:05:06 +0100

    my switch statment completed

--- control.java
+++ control.java
@@ -16,14 +16,38 @@ public class control {
 //System.out.println("your grade is B");
 //}else{
 //System.out.println("your grade is F sorry");
-String username = "admin";
-String password = "1234";
-if(username.equals("admin")&& password.equals("1234")){
-System.out.println("login successful");
-}else{
-System.out.println("invalid details");
-} 
-
+//String username = "admin";
+//String password = "1234";
+//if(username.equals("admin")&& password.equals("1234")){
+//System.out.println("login successful");
+//}else{
+//System.out.println("invalid details"};
+ 
+ int day = 10;
+switch (day){
+ case 1:
+System.out.println("its monday");
+break;
+ case 2:
+System.out.println("its teusday");
+break;
+ case 3:
+System.out.println("its wednesday");
+break;
+ case 4:
+System.out.println("its thursday");
+break;
+ case 5:
+System.out.println("its friday");
+break;
+ case 6:
+System.out.println("its saturday");
+break;
+ case 7:
+System.out.println("its sunday");
+ default:
+System.out.println("is not days of a week");
+}
 }
 
 }
