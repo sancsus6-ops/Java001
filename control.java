cf06baa33e9b07952ca65a6dd0fd6ad92ddeb2b2
Sancsus6-ops <sancsus6@gmail.com>
2026-04-13 20:31:01 +0100

    my for each

--- control.java
+++ control.java
@@ -23,32 +23,41 @@ public class control {
 //}else{
 //System.out.println("invalid details"};
  
- int day = 10;
-switch (day){
- case 1:
-System.out.println("its monday");
-break;
- case 2:
-System.out.println("its teusday");
-break;
- case 3:
-System.out.println("its wednesday");
-break;
- case 4:
-System.out.println("its thursday");
-break;
- case 5:
-System.out.println("its friday");
-break;
- case 6:
-System.out.println("its saturday");
-break;
- case 7:
-System.out.println("its sunday");
- default:
-System.out.println("is not days of a week");
+// int day = 10;
+//switch (day){
+// case 1:
+//System.out.println("its monday");
+//break;
+// case 2:
+//System.out.println("its teusday");
+//break;
+// case 3:
+//System.out.println("its wednesday");
+//break;
+// case 4:
+//System.out.println("its thursday");
+//break;
+// case 5:
+//System.out.println("its friday");
+//break;
+// case 6:
+//System.out.println("its saturday");
+//break;
+// case 7:
+//System.out.println("its sunday");
+//default:
+//System.out.println("is not days of a week");
+//}
+ 
+for(int e = 1; e <= 20; e++){
+if (e % 2 == 0){
+System.out.println(e);
+}
+//int e = 1;
+//do {
+//System.out.println(e);
+//e++; while(e <= 20; e % 2 ==0);
+//}
 }
 }
-
 }
-
